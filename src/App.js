d15b1a92f7cfc576b27f4defbd8565ef5e199901
qhwa <qhwa@163.com>
2019-11-18 07:29:37 +0800

    smooth value noise

--- src/App.js
+++ src/App.js
@@ -1,46 +1,93 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import generateNoise from './noise';
 import 'bulma/css/bulma.min.css';
 import './App.css';
 
 function App() {
   const canvasEl = useRef(null);
+  const [throt, setThrot] = useState(false);
+  const [t, setTime] = useState(0);
+
+  useEffect(() => {
+    refresh();
+  });
 
   return (
     <div className="App">
       <canvas ref={canvasEl} />
-      <form onSubmit={e => handleSubmit(e, canvasEl)}>
+      <form onSubmit={e => {
+        handleSubmit(e)
+      }}>
+        <div>
+          <label>
+            <input type="checkbox" value={throt} onChange={
+              e => setThrot(e.target.checked)
+            } />
+            throt
+          </label>
+        </div>
+      
+        <div>
+          <label>
+            <input
+              type="range"
+              max="100"
+              value={t}
+              onChange={e => {
+                setTime(e.target.value);
+              }}
+              style={{background: `linear-gradient(90deg, rgb(11, 30, 223) ${t}%, rgba(255, 255, 255, 0.216) ${t}%)`}}
+            />
+            {t}
+          </label>
+        </div>
+
         <input type="submit" value="generate" className="button" />
       </form>
     </div>
   );
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    refresh();
+  }
+
+  function refresh() {
+    const canvas = canvasEl.current;
+    const {width, height} = canvas;
+    const ctx = canvas.getContext('2d');
+
+    const image = generateImage(width, height, throt, t);
+    ctx.putImageData(image, 0, 0);
+  }
 }
 
-function handleSubmit(evt, ref) {
-  evt.preventDefault();
-
-  const canvas = ref.current;
-  const {width, height} = canvas;
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'green';
-  ctx.fillRect(0, 0, width, height);
-
-  const image = generateImage(width, height);
-  ctx.putImageData(image, 0, 0);
-}
-
-function generateImage(w, h) {
-  const data = generateNoise(w, h);
+function generateImage(w, h, throt, t) {
+  const grey = generateNoise(w, h, t);
   const numbers = Uint8ClampedArray.from({length: w * h * 4}, (_, k) => {
     const idx = k >> 2;
     const type = k % 4;
     if (type < 3) {
-      return data[idx];
+      return throt ?
+        uvTransform(grey[idx], [0, 158], [0, 255]) :
+        grey[idx];
     }
     return 255;
   });
 
   return new ImageData(numbers, w, h);
+}
+
+function uvTransform(n, [min, max], [minV, maxV]) {
+  if (n > max) {
+    return maxV;
+  }
+
+  if (n < min) {
+    return minV;
+  }
+
+  return 128;
 }
 
 export default App;
--- src/noise.js
+++ src/noise.js
@@ -1,10 +1,41 @@
-export default function generateNoise(w, h) {
-  return whiteNoise(w, h);
+export default function generateNoise(w, h, t = 0) {
+  return valueNoise(w, h, t);
 }
 
-function whiteNoise(w, h) {
+export function whiteNoise(w, h) {
   return Uint8ClampedArray.from({length: w * h}, () => {
     return Math.floor(Math.random() * 255);
   });
 }
 
+const GRID = 10; // 纹理划分 10 个格子
+const template = Array.from({length: GRID}, (_, k) => {
+  return Math.floor(Math.random() * 255);
+});
+
+export function valueNoise(w, h, t) {
+  return Uint8ClampedArray.from({length: w * h}, (_, k) => {
+    const SPEED = 2;
+    const x = normalize(k % w, Math.floor(t * SPEED), GRID);
+    const x0 = Math.floor(x);
+    const x1 = x0 === GRID - 1 ? 0 : x0 + 1;
+
+    return cosineRemap(template[x0], template[x1], x - x0);
+  });
+}
+
+function normalize(x, offset, grids) {
+  let n = ((x - offset) / 20 % grids);
+  if (n < 0) {
+    n += grids;
+  }
+  return n;
+}
+
+function cosineRemap(min, max, t) {
+  return lerp(min, max, (1 - Math.cos(t * Math.PI)) * 0.5);
+}
+
+function lerp(min, max, t) {
+  return min + (max - min) * t;
+}
