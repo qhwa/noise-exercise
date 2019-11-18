76688ed56e564bd4885020ab779a57f25cb1b5ca
qhwa <qhwa@163.com>
2019-11-18 08:29:51 +0800

    regroup code

--- src/noise.js
+++ src/noise.js
@@ -1,38 +1,5 @@
-export default function generateNoise(w, h, t = 0) {
-  return valueNoise1D(w, h, t);
-}
-
-export function whiteNoise(w, h) {
-  return Uint8ClampedArray.from({length: w * h}, () => {
-    return Math.floor(Math.random() * 255);
-  });
-}
-
-const GRID = 256; // 纹理划分 256 个格子
-const GRID_MASK = GRID - 1;
-const template = Array.from({length: GRID}, (_, k) => {
-  return Math.floor(Math.random() * 255);
-});
-
-export function valueNoise1D(w, h, t) {
-  return Uint8ClampedArray.from({length: w * h}, (_, k) => {
-    const SPEED = 2;
-    const x = normalize(k % w, Math.floor(t * SPEED)) % GRID;
-    const x0 = x & GRID_MASK;
-    const x1 = x0 + 1;
-
-    return cosineRemap(template[x0], template[x1], x - x0);
-  });
-}
-
-function normalize(x, offset) {
-  return (x + offset) / 10;
-}
-
-function cosineRemap(min, max, t) {
-  return lerp(min, max, (1 - Math.cos(t * Math.PI)) * 0.5);
-}
-
-function lerp(min, max, t) {
-  return min + (max - min) * t;
+export default function generateNoise(type, w, h, t = 0) {
+  return import(`./noise/${type}`).then(
+    g => g.default.call(null, w, h, t)
+  );
 }
---
+++ src/noise/white_noise.js
@@ -0,0 +1,6 @@
+export default function whiteNoise(w, h) {
+  return Uint8ClampedArray.from({length: w * h}, () => {
+    return Math.floor(Math.random() * 255);
+  });
+}
+
