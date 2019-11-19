cd25527f8e7d9c0294076ef602f1c2383b52c3ad
qhwa <qhwa@163.com>
2019-11-19 15:00:41 +0800

    improve label

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
               <option value="white_noise">white noise</option>
               <option value="value_noise_1d">value noise 1D</option>
               <option value="value_noise_2d">value noise 2D</option>
-              <option value="perlin_noise_2d">Perlin noise 2D</option>
+              <option value="perlin_noise_2d">original Perlin noise 2D</option>
             </select>
           </div>
         </div>
